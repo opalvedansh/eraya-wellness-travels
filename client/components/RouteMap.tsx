b68e8cc0b604a965bec360ec7070a7c84b3df795
opalvedansh <vedanshlovesmom88@gmail.com>
2026-01-25 02:41:58 +0530

    fix: use CDN urls for leaflet markers to ensure visibility

--- client/components/RouteMap.tsx
+++ client/components/RouteMap.tsx
@@ -8,9 +8,9 @@ function MapConfig() {
             // Fix for default marker icons
             delete (L.Icon.Default.prototype as any)._getIconUrl;
             L.Icon.Default.mergeOptions({
-                iconRetinaUrl: '/assets/leaflet/marker-icon-2x.png',
-                iconUrl: '/assets/leaflet/marker-icon.png',
-                shadowUrl: '/assets/leaflet/marker-shadow.png',
+                iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+                iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+                shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
             });
         });
     }, []);
